3f5709c10947c4907e6fd06b1ad50d64af83fb23
TRR-a <1458142677@qq.com>
2026-08-22 19:21:13 +0800

    chore: increase commitlint header max length to 1000

--- commitlint.config.js
+++ commitlint.config.js
@@ -20,6 +20,6 @@ module.exports = {
     ],
     'subject-case': [0],
     'subject-full-stop': [0],
-    'header-max-length': [2, 'always', 100]
+    'header-max-length': [2, 'always', 1000]
   }
 }
